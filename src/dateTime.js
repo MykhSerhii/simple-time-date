a071c892b9957040dff8c0f53ec48109305042bc
Mykhailenko Serhii <newsklog@gmail.com>
2023-05-27 22:12:25 +0300

    update dateTime, add try-catch

--- src/dateTime.js
+++ src/dateTime.js
@@ -1,38 +1,44 @@
-import {isInteger} from "./helpers"
+import {isInteger, isString} from "./helpers"
 
+const DEFAULT_FORMAT = 'Y.M.D H:m:s'
 
-export function setFormat(dateTime, timeFormat = 'Y.M.D H:m:s') {
+export function setFormat(dateTime, timeFormat = DEFAULT_FORMAT) {
     let newDateTime = ''
     for (let i = 0; i < timeFormat.length; i++) {
         let letter = timeFormat[i]
-        switch (letter) {
-            case 'D':
-                letter = dateTime.getUTCDate()
-                break
-            case 'M':
-                letter = dateTime.getUTCMonth() + 1
-                break
-            case 'Y':
-                letter = dateTime.getUTCFullYear()
-                break
-            case 'H':
-                letter = dateTime.getUTCHours()
-                break
-            case 'm':
-                letter = dateTime.getUTCMinutes()
-                break
-            case 's':
-                letter = dateTime.getUTCSeconds()
-                break
-            case 'S':
-                letter = dateTime.getUTCMilliseconds()
-                if (letter < 100) {
-                    letter = '0' + letter
-                }
-                if (letter < 10) {
-                    letter = '0' + letter
-                }
-                break
+        try {
+            switch (letter) {
+                case 'D':
+                    letter = dateTime.getUTCDate()
+                    break
+                case 'M':
+                    letter = dateTime.getUTCMonth() + 1
+                    break
+                case 'Y':
+                    letter = dateTime.getUTCFullYear()
+                    break
+                case 'H':
+                    letter = dateTime.getUTCHours()
+                    break
+                case 'm':
+                    letter = dateTime.getUTCMinutes()
+                    break
+                case 's':
+                    letter = dateTime.getUTCSeconds()
+                    break
+                case 'S':
+                    letter = dateTime.getUTCMilliseconds()
+                    if (letter < 100) {
+                        letter = '0' + letter
+                    }
+                    if (letter < 10) {
+                        letter = '0' + letter
+                    }
+                    break
+            }
+        } catch (err) {
+            console.error(`Set time format error! Error: ${err.message}`)
+            return ''
         }
         if (typeof letter === 'number' && letter < 10) {
             letter = '0' + letter
@@ -43,21 +49,48 @@ export function setFormat(dateTime, timeFormat = 'Y.M.D H:m:s') {
 }
 
 export function setZone(dateTime, timeZone = '') {
-    const zone = Number(timeZone.slice(1))
-    if (isInteger(zone)) {
-        if (timeZone[0] === '+') {
-            dateTime.setUTCHours(dateTime.getUTCHours() + zone)
-        } else if (timeZone[0] === '-') {
-            dateTime.setUTCHours(dateTime.getUTCHours() - zone)
-        }
+    if (!isString(timeZone)) {
+        console.error(`Invalid time zone: ${timeZone}! Necessary string.`)
+        return ''
     }
-    return dateTime
+    const zone = Number(timeZone.slice(1))
+    try {
+        if (isInteger(zone)) {
+            if (timeZone[0] === '+') {
+                dateTime.setUTCHours(dateTime.getUTCHours() + zone)
+            } else if (timeZone[0] === '-') {
+                dateTime.setUTCHours(dateTime.getUTCHours() - zone)
+            }
+        }
+        return dateTime
+    } catch (err) {
+        console.error(`Set time zone error! Error: ${err.message}`)
+        return ''
+    }
 }
 
 export class DateTime {
-    now() {
+    #format
+    #zone
+
+    constructor(format, zone) {
+        this.#format = format ?? DEFAULT_FORMAT
+        this.#zone = zone ?? ''
+    }
+
+    now(format) {
         const time = new Date()
-        return setFormat(setZone(time))
+        return setFormat(setZone(time, this.#zone), format ?? this.#format)
+    }
+
+    create(timeString, format) {
+        let time
+        try {
+            time = new Date(timeString)
+        } catch {
+            console.error(`Invalid time string: ${timeString}`)
+            return ''
+        }
+        return setFormat(setZone(time, this.#zone),format ?? this.#format)
     }
 }
-
--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,7 @@
 export function isInteger(value) {
     return Number.isInteger(Number(value))
 }
+
+export function isString(value) {
+    return typeof value === 'string' && value.trim !== ''
+}
